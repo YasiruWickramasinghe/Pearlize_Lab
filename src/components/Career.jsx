2be7990e4494cb73105831120654c110c6ad0c4c
YasiruWickramasinghe <it19038292@my.sliit.lk>
2023-08-05 08:37:33 +0530

    Structure update

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -8,6 +8,11 @@ const Careers = () => {
         <h3 className="text-4xl font-semibold">
         Become a <span className="text-gray-900">Pearlian</span>
         </h3>
+        {/* <p>Explore remote-friendly, flexible opportunities and join our mission to build great tech alongside a bunch of awesome, dedicated and passionate Rootcoders.</p> */}
+        {/* <button>View Operning</button> */}
+        {/* image */}
+        {/* interview process */}
+        {/* four step screening, assesment, technical interview, hr interview, offer */}
       </div>
     </section>
   );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,14 @@ const Contact = () => {
     <section id="contact" className="py-10 px-3 text-white">
       <div className="text-center mt-8">
         <h3 className="text-4xl font-semibold">
-         <span className="text-yellow-500">Contact </span>Us
+         Let's <span className="text-yellow-500">Talk</span>
         </h3>
+        {/* <p>Book a Free consultation with one of our specialists, so that we can discuss how Pearlize Lab can help your business to build the product you have in mind.</p> */}
+        {/* <button>Book Consultation</button> */}
+        {/* <h3 className="text-4xl font-semibold">
+        Send us a message
+        </h3> */}
+        {/* <form action=""></form> */}
       </div>
     </section>
   );
